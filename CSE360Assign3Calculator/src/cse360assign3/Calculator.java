a4117164bfd33fa919a628ea9ef9efb9cb7b5c41
Michael Fraiz <mfraiz@asu.edu>
2016-02-24 15:09:34 -0700

    Initial Commit of Calculator Class + JUNIT tests

---
+++ CSE360Assign3Calculator/src/cse360assign3/Calculator.java
@@ -0,0 +1,88 @@
+
+package cse360assign3;
+
+/** Calculator.java
+ * This class represents a calculator that performs basic int operations (Add,Subtract,Divide,Multiply) on integers and stores the result as
+ * an int. This class also keeps a log of all operations performed and can return this log in the form of a string. 
+*
+* @author Michael Fraiz
+* @version Feb 24 2016.
+*/
+
+public class Calculator {
+
+	//Private value that represents the current int value stored in the calculator for calculations. 
+	private int total;
+	
+	/**Calculator()
+	 * Default constructor that creates a new calculator object with a default of 0 and an empty log String. 
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** public int getTotal()
+	*
+	* @return total The integer value that corresponds to the current value held by
+	* the calculator class.
+	* 
+	* This method returns the current total value stored in the calculator as an int.
+	* Defaults to 0 if no calculations have been performed
+	*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** public void add(int value)
+	*
+	* @param int value 				value to be added to total
+	* This performs the addition operation, adding the param to the stored total
+	* * Stores the resulting calculation back into total.
+	*/
+	public void add (int value) {
+		
+	}
+	
+	/** public void subtract(int value)
+	*
+	* @param int value 				value to be subtracted from total
+	* This performs the subtraction operation, subtracting the param from the stored total
+	* * Stores the resulting calculation back into total.
+	*/
+	public void subtract (int value) {
+		
+	}
+	
+	/** public void multiply (int value)
+	*
+	* @param int value 				value to be multiplied with total
+	* This performs the multiplication operation and multiplies the param with the stored total
+	* Stores the resulting calculation back into total. 
+	*/
+	public void multiply (int value) {
+		
+	}
+	
+	/** public void divide (int value)
+	*
+	* @param int value 				value that total is divided by
+	* This performs the integer division operation and divides the total by parameter value.
+	* Stores the resulting calculation back into total. 
+	*/
+	public void divide (int value) {
+		
+	}
+	
+	/** public String getHistory ()
+	*
+	* @return String   This string contains the history of operations performed by that particular instance of the calculator class.
+	
+		This method returns the history of operations performed by the calculator class as a series of characters in a String.
+		For example, if you subtract 5 then multiply by 2, this will return "- 5 * 2" and so on.
+		Additionally, for purposes of this class, dividing by zero will reset the total to zero instead of NaN.
+	*/
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ CSE360Assign3Calculator/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,185 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+//This JUNIT test ensures that the methods of Calculator.java are working correctly.
+public class CalculatorTest 
+{
+
+	@Test
+	public void testConstructor() 
+	{
+		//Ensure that Constructor creates an object that is not null.
+		Calculator newCalculator = new Calculator();
+		
+		assertNotNull(newCalculator);
+	}
+
+	@Test
+	public void testGetTotal() 
+	{
+		fail("Not yet implemented");
+		//Test to make sure total is zero as default
+		Calculator newCalculator = new Calculator();
+		
+		assertEquals(0,newCalculator.getTotal());
+	}
+	
+	@Test
+	public void testAddition() 
+	{
+		fail("Not yet implemented");
+		
+		//Test Addition Operations
+		Calculator newCalculator = new Calculator();
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.add(5);
+		assertEquals(5,newCalculator.getTotal());
+		
+		newCalculator.add(5);
+		assertEquals(10,newCalculator.getTotal());
+		
+		newCalculator.add(-10);
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.add(-100);
+		assertEquals(-100,newCalculator.getTotal());
+		
+		newCalculator.add(350);
+		assertEquals(250,newCalculator.getTotal());
+		
+		//Check add zero
+		newCalculator.add(0);
+		assertEquals(250,newCalculator.getTotal());
+		
+	}
+	
+	@Test
+	public void testSubtraction() 
+	{
+		fail("Not yet implemented");
+		
+		//Test Subtraction Operations
+		Calculator newCalculator = new Calculator();
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.subtract(5);
+		assertEquals(-5,newCalculator.getTotal());
+		
+		newCalculator.subtract(-5);
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.subtract(-70);
+		assertEquals(70,newCalculator.getTotal());
+		
+		newCalculator.subtract(35);
+		assertEquals(35,newCalculator.getTotal());
+		
+		newCalculator.subtract(35);
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.subtract(20);
+		assertEquals(-20,newCalculator.getTotal());
+		
+		newCalculator.subtract(0);
+		assertEquals(-20,newCalculator.getTotal());
+	}
+	
+	@Test
+	public void testMultiplication() 
+	{
+		fail("Not yet implemented");
+		
+		//Test Multiplication Operations
+		Calculator newCalculator = new Calculator();
+		assertEquals(0,newCalculator.getTotal());
+				
+		newCalculator.multiply(5);
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.add(2);
+		assertEquals(2,newCalculator.getTotal());
+		
+		newCalculator.multiply(2);
+		assertEquals(4,newCalculator.getTotal());
+		
+		newCalculator.multiply(2);
+		assertEquals(8,newCalculator.getTotal());
+		
+		newCalculator.multiply(2);
+		assertEquals(16,newCalculator.getTotal());
+		
+		newCalculator.multiply(2);
+		assertEquals(32,newCalculator.getTotal());
+		
+		newCalculator.multiply(2);
+		assertEquals(64,newCalculator.getTotal());
+		
+		newCalculator.multiply(1);
+		assertEquals(64,newCalculator.getTotal());
+		
+		newCalculator.multiply(100);
+		assertEquals(6400,newCalculator.getTotal());
+		
+		newCalculator.multiply(0);
+		assertEquals(0,newCalculator.getTotal());
+	}
+	
+	@Test
+	public void testDivision() 
+	{
+		fail("Not yet implemented");
+		
+		//Test Multiplication Operations
+		Calculator newCalculator = new Calculator();
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.divide(5);
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.add(10);
+		assertEquals(10,newCalculator.getTotal());
+		
+		newCalculator.divide(5);
+		assertEquals(2,newCalculator.getTotal());
+		
+		newCalculator.divide(2);
+		assertEquals(1,newCalculator.getTotal());
+		
+		newCalculator.divide(1);
+		assertEquals(1,newCalculator.getTotal());
+		
+		//Test Special Case, Divide by zero will return zero
+		newCalculator.divide(0);
+		assertEquals(0,newCalculator.getTotal());
+		
+		//Test integer division logic, no remainder
+		newCalculator.add(40);
+		newCalculator.divide(30);
+		assertEquals(1,newCalculator.getTotal());
+		
+	}
+	
+	@Test
+	public void testGetHistory() 
+	{
+		fail("Not yet implemented");
+		
+		//Test Multiplication Operations
+		Calculator newCalculator = new Calculator();
+		assertEquals(0,newCalculator.getTotal());
+		
+		newCalculator.add(5);
+		newCalculator.subtract(3);
+		newCalculator.multiply(20);
+		newCalculator.divide(10);
+		
+		assertEquals(4,newCalculator.getTotal());
+		assertEquals("+ 5 - 3 * 20 / 10",newCalculator.getHistory());
+		
+		
+	}
+}
